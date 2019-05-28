211b55eb11ec5cbae333bb66ccbae999092ccf78
meg768 <meg768@users.noreply.github.com>
2019-05-28 17:12:42 +0200

    -

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+var ws281x = require('rpi-ws281x');
+var Pixels = require('rpi-pixels');
+
+
 module.exports.Strip = require('./src/strip.js');
 module.exports.Matrix = require('./src/matrix.js');
 module.exports.Animation = require('./src/animation.js');
 module.exports.AnimationQueue = require('./src/animation-queue.js');
+
+
+var WIDTH = undefined;
+var HEIGHT = undefined;
+
+module.exports.configure = function(options) {
+
+    var {width, height, ...other} = options;
+
+    WIDTH = width;
+    HEIGHT = height;
+
+    ws281x.configure({leds: width * height, ...other});
+}
+
+module.exports.Pixels = class extends Pixels {
+
+    constructor(options = {}) {
+        super({...options, width:WIDTH, height:HEIGHT});
+    }
+
+    render(options) {
+        ws281x.render(this.pixels);
+    }
+
+}
---
+++ test.js
@@ -0,0 +1,25 @@
+var Neopixels = require('./index.js');
+
+
+class App {
+
+    constructor() {
+
+        this.pixels = new Neopixels();
+
+    }
+
+    run() {
+        this.pixels.fillRGB(255, 0, 0);
+
+
+    }
+}
+
+
+
+Neopixels.configure({width:13, height:13});
+
+var app = new App();
+app.run();
+
