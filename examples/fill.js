80c432987228766ea9a6d989caa48b38fa4b32bb
meg768 <meg768@users.noreply.github.com>
2019-05-30 00:25:20 +0200

    -

---
+++ examples/fill.js
@@ -0,0 +1,29 @@
+var Neopixels = require('./neopixels.js');
+
+
+class App {
+
+    constructor() {
+
+        this.pixels = new Neopixels.Pixels();
+        this.offset = 0;
+
+    }
+
+    run() {
+        this.pixels.fill('red');
+        this.pixels.render({transition:'fade', duration:500});
+        this.pixels.fill('green');
+        this.pixels.render({transition:'fade', duration:500});
+        this.pixels.fill('blue');
+        this.pixels.render({transition:'fade', duration:500});
+        this.pixels.fill('black');
+        this.pixels.render({transition:'fade', duration:500});
+
+    }
+}
+
+
+var app = new App();
+app.run();
+
---
+++ examples/sequence.js
@@ -0,0 +1,32 @@
+var Neopixels = require('../neopixels.js');
+
+class App {
+
+    constructor() {
+
+        this.pixels = new Neopixels.Pixels();
+        this.offset = 0;
+
+    }
+
+    loop() {
+        var x = this.offset % this.pixels.width;
+        var y = Math.floor(this.offset / this.pixels.width);
+
+        this.pixels.clear();
+        this.pixels.setPixelRGB(x, y, 255, 255, 255);
+        this.pixels.render();
+
+        this.offset = (this.offset + 1) % (this.pixels.width * this.pixels.height);
+
+    }
+
+    run() {
+        setInterval(this.loop.bind(this), 100);
+    }
+}
+
+
+var app = new App();
+app.run();
+
