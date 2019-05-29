d7b5c644bf152749bc3c2f000fef04e78dbc4de0
meg768 <meg768@users.noreply.github.com>
2019-05-30 00:48:09 +0200

    -

--- examples/neopixels.js
+++ examples/neopixels.js
@@ -2,6 +2,24 @@ var Neopixels = require('../index.js');
 
 function configure() {
 
+    function installCleanup() {
+
+        function cleanup() {
+            var pixels = new Neopixels.Pixels();
+    
+            pixels.fill('black');
+            pixels.render();
+            
+            process.exit();
+        }
+    
+        process.on('SIGUSR1', cleanup);
+        process.on('SIGUSR2', cleanup);
+        process.on('SIGINT',  cleanup);
+        process.on('SIGTERM', cleanup);
+    
+    }
+    
     var stripType = 'grb';
     var width     = 13;
     var height    = 13;
@@ -9,28 +27,11 @@ function configure() {
 
     Neopixels.configure({debug:false, map:map, width:width, height:height, stripType:stripType});
 
-}
-
-function installCleanup() {
-
-	function cleanup() {
-        var pixels = new Neopixels.Pixels();
-
-        pixels.fill('black');
-        pixels.render();
-        
-        process.exit();
-	}
-
-	process.on('SIGUSR1', cleanup);
-	process.on('SIGUSR2', cleanup);
-	process.on('SIGINT',  cleanup);
-	process.on('SIGTERM', cleanup);
+    installCleanup();
 
 }
 
 
 configure();
-installCleanup();
 
 module.exports = Neopixels;
